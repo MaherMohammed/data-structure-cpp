6f2f0a50bd3deaeaff5ec51fa554e361045f6782
MaherMohammed <maher.mohammed10.97@gmail.com>
2021-06-18 19:00:07 +0200

    linked list

--- dataStructureProject.cpp
+++ dataStructureProject.cpp
@@ -115,113 +115,201 @@ int queue:: dequeue() //delete first in linked list
         return item;
     }
 
+
+class linked_list
+{
+    private:
+    node *start; node * last;
+    public:
+    linked_list()
+    { start=NULL; last= NULL;}
+    void insert_last(int value);
+    void insert_first(int value) ;
+    void insert_at_position(int pos, int value) ;
+    void display();
+}; 
+
+
+
+void linked_list:: insert_last(int value)
+{
+    node *temp=new node;
+    temp->data=value;
+    temp->next=NULL;
+    if (start == NULL)
+    {
+        start=temp;
+        last=temp;
+    }
+    else
+    {
+        last->next = temp;
+        last = temp;
+    }
+}
+
+void linked_list::insert_first(int value)
+{
+    node *temp=new node;
+    temp->data=value;
+    temp->next=NULL;
+    if (start == NULL)
+    {
+        start=temp;
+        last=temp;
+    }
+    else{
+        temp->next=start;
+        start=temp;
+    }
+}
+
+void linked_list:: display()
+{ if(start==NULL) {cout<<"list is empty "; return;}
+cout<<" the elements of the list are "<<endl;
+node *temp=start;
+while(temp!=NULL)
+{
+cout<<temp->data<<"\t";
+temp=temp->next;
+}
+} 
+
+
+// void linked_list::insert_at_position(int pos, int value)
+//     {node *temp=new node;
+//     temp->data=value;
+//     temp->next=NULL;
+//     node *current=start;
+//     for(int i=1;i<pos-1;i++)
+//     {
+//     current=current->next;
+//     if(current==NULL)
+//     {cout<<" the list has element less than
+//     "<<pos<< "element ";return;}
+//     }
+//     temp->next=current->next;
+// current->next=temp;
+// }
+
 int main()
 
 {
 
-    displayMainMenu();
+    linked_list l;
+    l.insert_last(2);
+    l.insert_first(1);
+    l.display();
+    cout<<""<<endl;
 
-    int input = 0;
-    cout<<"please enter your choice: ";
-    cin>>input;
+    // displayMainMenu();
 
-    while (!(input >= 5))
-    {
-        int numberOfItems;
-        if (input == 1)
-        {
-            //implement stack using queue
-            queue q1;
-            queue q2;
-            cout<<"please enter number of items you have: ";
-            cin>>numberOfItems;
-            int saveNumber = numberOfItems;
-            int index = 1;
-            //fill the first queue
-            while (index <= numberOfItems)
-            {
-                int item;
-                cout<<"please enter item number ";
-                cout<<index;
-                cout<<": ";
-                cin>>item;
-                q1.enqueue(item);
-                index++;
-            }
+    // int input = 0;
+    // cout<<"please enter your choice: ";
+    // cin>>input;
+
+    // while (!(input >= 5))
+    // {
+    //     int numberOfItems;
+    //     if (input == 1)
+    //     {
+    //         //implement stack using queue
+    //         queue q1;
+    //         queue q2;
+    //         cout<<"please enter number of items you have: ";
+    //         cin>>numberOfItems;
+    //         int saveNumber = numberOfItems;
+    //         int index = 1;
+    //         //fill the first queue
+    //         while (index <= numberOfItems)
+    //         {
+    //             int item;
+    //             cout<<"please enter item number ";
+    //             cout<<index;
+    //             cout<<": ";
+    //             cin>>item;
+    //             q1.enqueue(item);
+    //             index++;
+    //         }
             
-            index = 1;
-            while (index <= numberOfItems)
-            {
-                if (numberOfItems == index)
-                {
-                    q2.enqueue(q1.dequeue());
-                    numberOfItems--;
-                    index = 1;
-                }else
-                {
-                    q1.enqueue(q1.dequeue());
-                    index ++;
-                }
-            }
+    //         index = 1;
+    //         while (index <= numberOfItems)
+    //         {
+    //             if (numberOfItems == index)
+    //             {
+    //                 q2.enqueue(q1.dequeue());
+    //                 numberOfItems--;
+    //                 index = 1;
+    //             }else
+    //             {
+    //                 q1.enqueue(q1.dequeue());
+    //                 index ++;
+    //             }
+    //         }
 
-            index = 0;
-            while (index < saveNumber)
-            {
-                cout<<q2.dequeue()<<endl;
-                index++;
-            }
-            
-            
-
-
-        }else if (input == 2)
-        {
-            stack s;
-            cout<<"please enter number of items you have: ";
-            cin>>numberOfItems;
-            int index = 1;
-            while (index <= numberOfItems)
-            {
-                //print enter items
-                int item;
-                cout<<"please enter item number ";
-                cout<<index;
-                cout<<": ";
-                cin>>item;
-                //push the item to the stack
-                s.push(item);
-                //increase the index
-                index++;
-             //   cout<<index<<endl;
-            }
-            //initialize another stack to implement the queue
-            stack outputStack;
-            //push the other stack to it
-            while (!s.isEmpty())
-            {
-                int i = s.pop();
-                outputStack.push(i);
-            }
-            //print the outputstack as the queue
-            cout<<"Queue vlaues are"<<endl;
-            while (!outputStack.isEmpty())
-            {
-                cout<<outputStack.pop();
-                cout<<" ";
-            }
+    //         index = 0;
+    //         while (index < saveNumber)
+    //         {
+    //             cout<<q2.dequeue()<<endl;
+    //             index++;
+    //         }
             
             
-        }    
 
-        cout<<""<<endl;
-        cin.ignore();
-        cout<<"hit enter to return";
-        cin.get();
-        displayMainMenu();
-        cout<<"please enter your choice: ";
-        cin>>input;    
-        cout<<""<<endl;
-    }
+
+    //     }else if (input == 2)
+    //     {
+    //         stack s;
+    //         cout<<"please enter number of items you have: ";
+    //         cin>>numberOfItems;
+    //         int index = 1;
+    //         while (index <= numberOfItems)
+    //         {
+    //             //print enter items
+    //             int item;
+    //             cout<<"please enter item number ";
+    //             cout<<index;
+    //             cout<<": ";
+    //             cin>>item;
+    //             //push the item to the stack
+    //             s.push(item);
+    //             //increase the index
+    //             index++;
+    //          //   cout<<index<<endl;
+    //         }
+    //         //initialize another stack to implement the queue
+    //         stack outputStack;
+    //         //push the other stack to it
+    //         while (!s.isEmpty())
+    //         {
+    //             int i = s.pop();
+    //             outputStack.push(i);
+    //         }
+    //         //print the outputstack as the queue
+    //         cout<<"Queue vlaues are"<<endl;
+    //         while (!outputStack.isEmpty())
+    //         {
+    //             cout<<outputStack.pop();
+    //             cout<<" ";
+    //         }
+            
+            
+    //     }  
+    //     else if (input == 3)
+    //     {
+            
+    //     }
+          
+
+    //     cout<<""<<endl;
+    //     cin.ignore();
+    //     cout<<"hit enter to return";
+    //     cin.get();
+    //     displayMainMenu();
+    //     cout<<"please enter your choice: ";
+    //     cin>>input;    
+    //     cout<<""<<endl;
+    // }
 
 
 
